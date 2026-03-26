d5678ceca305d1a19c9047ba6f9beb7b7359ef8c
Ferdinand1195450 <1195450@student.roc-nijmegen.nl>
2026-03-26 14:29:44 +0100

    Quick fix
    Fixed the filepath and added the card png's

--- obj/Debug/net8.0-windows/Blackjack.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Blackjack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c43b39f82dfc1418ef810289dc762db2b3813bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
