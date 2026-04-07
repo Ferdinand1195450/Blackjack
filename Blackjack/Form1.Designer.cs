e2370e2561af7e521cd44d7c3bcdc8e2b66e5ec3
Ferdinand1195450 <1195450@student.roc-nijmegen.nl>
2026-04-07 11:33:54 +0200

    feat:
    shuffle en draw toegevoegd

--- Blackjack/Form1.Designer.cs
+++ Blackjack/Form1.Designer.cs
@@ -28,12 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            buttonShuffle = new Button();
+            buttonDraw = new Button();
+            textBox1 = new TextBox();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // buttonShuffle
+            // 
+            buttonShuffle.Location = new Point(224, 239);
+            buttonShuffle.Name = "buttonShuffle";
+            buttonShuffle.Size = new Size(94, 29);
+            buttonShuffle.TabIndex = 0;
+            buttonShuffle.Text = "Shuffle";
+            buttonShuffle.UseVisualStyleBackColor = true;
+            buttonShuffle.Click += buttonShuffle_Click;
+            // 
+            // buttonDraw
+            // 
+            buttonDraw.Location = new Point(346, 239);
+            buttonDraw.Name = "buttonDraw";
+            buttonDraw.Size = new Size(94, 29);
+            buttonDraw.TabIndex = 1;
+            buttonDraw.Text = "Draw";
+            buttonDraw.UseVisualStyleBackColor = true;
+            buttonDraw.Click += buttonDraw_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(214, 89);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(483, 46);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(81, 117);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox1);
+            Controls.Add(buttonDraw);
+            Controls.Add(buttonShuffle);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button buttonShuffle;
+        private Button buttonDraw;
+        private TextBox textBox1;
+        private PictureBox pictureBox1;
     }
 }
--- Blackjack/Form1.cs
+++ Blackjack/Form1.cs
@@ -2,9 +2,69 @@ namespace Blackjack
 {
     public partial class Form1 : Form
     {
+        private List<string> deck = new List<string>();
+        private Random rng = new Random();
+
         public Form1()
         {
             InitializeComponent();
+            MaakDeck();
+        }
+
+        private void MaakDeck()
+        {
+            string[] suits = { "clubs", "diamonds", "hearts", "spades" };
+            string[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace" };
+
+            deck.Clear();
+
+            foreach (string suit in suits)
+            {
+                foreach (string rank in ranks)
+                {
+                    deck.Add($"{rank}_of_{suit}.png");
+                }
+            }
+        }
+
+        private void Shuffle()
+        {
+            for (int i = deck.Count - 1; i > 0; i--)
+            {
+                int j = rng.Next(i + 1);
+
+                string temp = deck[i];
+                deck[i] = deck[j];
+                deck[j] = temp;
+            }
+        }
+
+        private void buttonShuffle_Click(object sender, EventArgs e)
+        {
+            Shuffle();
+            textBox1.Text = "Deck geschud";
+        }
+
+        private void buttonDraw_Click(object sender, EventArgs e)
+        {
+            if (deck.Count == 0)
+            {
+                textBox1.Text = "Geen kaarten meer";
+                return;
+            }
+
+            string card = deck[0];
+            deck.RemoveAt(0);
+
+            textBox1.Text = card;
+
+            // als je picturebox gebruikt:
+            string path = System.IO.Path.Combine(Application.StartupPath, "Cards", card);
+
+            if (System.IO.File.Exists(path))
+            {
+                pictureBox1.Image = System.Drawing.Image.FromFile(path);
+            }
         }
     }
 }
